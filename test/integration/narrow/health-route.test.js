94fd5b7c674720494cc1300a0b83286c47f8942f
Rana Salem <ranatasalem@gmail.com>
2026-04-17 11:52:08 +0100

    update tests

--- test/integration/narrow/health-route.test.js
+++ test/integration/narrow/health-route.test.js
@@ -1,8 +1,16 @@
-import { vi, describe, beforeEach, afterEach, test, expect } from 'vitest'
+import { describe, beforeEach, afterEach, test, expect } from 'vitest'
+import http2 from 'node:http2'
 import { createServer } from '../../../src/server/create-server.js'
+
+const { constants: httpConstants } = http2
 
 describe('health route', () => {
   let server
+
+  const options = {
+    method: 'GET',
+    url: '/health'
+  }
 
   beforeEach(async () => {
     server = await createServer()
@@ -14,24 +22,14 @@ describe('health route', () => {
   })
 
   test('GET /health returns 200', async () => {
-    const options = {
-      method: 'GET',
-      url: '/health'
-    }
-
     const response = await server.inject(options)
 
-    expect(response.statusCode).toBe(200)
+    expect(response.statusCode).toBe(httpConstants.HTTP_STATUS_OK)
   })
 
-  test('GET /health returns success', async () => {
-    const options = {
-      method: 'GET',
-      url: '/health'
-    }
-
+  test('GET /health returns success message', async () => {
     const response = await server.inject(options)
-    const message = JSON.parse(response.payload).message
+    const { message } = JSON.parse(response.payload)
 
     expect(message).toBe('success')
   })
